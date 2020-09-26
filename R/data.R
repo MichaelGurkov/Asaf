e04dcdb4b483c00f05d7dad93df63342dc52f610
MichaelGurkov <michael.gurkov@mail.huji.ac.il>
2020-09-26 10:39:03 +0300

    commit data

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#' Financial variables for S&P prediction
+#'
+#' @format A data frame with 7944 rows and 24 variables
+#'
+#' \describe{
+#'   \item{date}{dates}
+#'   \item{spx_index}{S&P 500 index, in points}
+#' }
+#'
+#'
+"sp_data"
